e6deb286d57035598388f55f36732bb4625ee33c
Ha Le <halntit@gmail.com>
2023-07-13 23:32:45 +0700

    add home + bands tests

---
+++ base-concert-venue/__tests__/ui/band.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import BandComponent from "@/pages/bands/[bandId]";
+import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+
+test("band component display correct band information", async () => {
+    const { fakeBands } = await readFakeData();
+    render(<BandComponent error={null} band={fakeBands[0]} />);
+
+    const heading = screen.getByRole("heading", {
+        name: /the wandering bunnies/i,
+    });
+    expect(heading).toBeInTheDocument();
+
+    // should write more tests here
+});
+
+test("band component display error", async () => {
+    render(<BandComponent error={"Error occurred"} band={null} />);
+
+    const error = screen.getByRole("heading", {
+        name: /error occurred/i,
+    });
+    expect(error).toBeInTheDocument();
+
+    // should write more tests here
+});
---
+++ base-concert-venue/__tests__/ui/home.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Home from '@/pages/index';
+
+it('page has correct heading and image', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+        name: /Welcome to Popular Concert Venue/i,
+    });
+    expect(heading).toBeInTheDocument();
+
+    const image = screen.getByRole('img', {
+        name: /concert goer with hands in the shape of a heart/i,
+    });
+    expect(image).toBeInTheDocument();
+})
