50d04b3db378507c8ba97601af2f56da55e3f0f0
Ha Le <halntit@gmail.com>
2023-07-29 23:36:27 +0700

    fix test (using e2e, not component testing)

---
+++ base-concert-venue/cypress/e2e/routes.test.cy.js
@@ -0,0 +1,19 @@
+it("displays correct heading when navigating to shows", () => {
+    cy.visit("/"); // visit base-concert-venue
+    cy.findByRole("button", {
+        name: /shows/i
+    }).click(); // don't need to "await", it automatically waits for page to load
+    cy.findByRole("heading", {
+        name: /upcoming shows/i
+    }).should('exist');
+});
+
+it("displays correct heading when navigating to bands", () => {
+    cy.visit("/");
+    cy.findByRole("button", {
+        name: /bands/i
+    }).click();
+    cy.findByRole("heading", {
+        name: /Our Illustrious Performers/i
+    }).should('exist');
+});
--- base-concert-venue/cypress/integration/routes.test.js
+++
@@ -1,9 +0,0 @@
-it("displays correct heading when navigating to shows", () => {
-    cy.visit("/");
-    cy.findByRole("button", {
-        name: /shows/i
-    }).click();
-    cy.findByRole("heading", {
-        name: /upcomming shows/i
-    }).should('exist');
-})
