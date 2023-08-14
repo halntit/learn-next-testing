4406eca8993906e2b72e219180da14db898b0f2f
Ha Le <halntit@gmail.com>
2023-08-14 18:23:41 +0700

    mock unauthorize

--- base-concert-venue/__tests__/api/reservations.test.ts
+++ base-concert-venue/__tests__/api/reservations.test.ts
@@ -1,8 +1,10 @@
 import { testApiHandler } from 'next-test-api-route-handler';
 import reversationsHandler from '@/pages/api/reservations/[reservationId]';
 import userReservationHandler from '@/pages/api/users/[userId]/reservations';
+import { validateToken } from '@/lib/auth/utils';
 
 jest.mock('@/lib/auth/utils');
+const validateTokenMock = validateToken as jest.Mock;
 
 describe("test reservations", () => {
     it('POST /api/reservations/[reservationId] create a new reservation', async () => {
@@ -41,6 +43,32 @@ describe("test reservations", () => {
 
                 const json = await res.json();
                 expect(json.userReservations).toHaveLength(3);
+            }
+        });
+    });
+
+    it("POST to /users/[userId]/reservations fails with 401", async () => {
+        validateTokenMock.mockResolvedValue(false);
+
+        await testApiHandler({
+            handler: reversationsHandler,
+            paramsPatcher: (params) => {
+                params.reservationId = 12354;
+            },
+            test: async ({ fetch }) => {
+                const res = await fetch({
+                    method: 'POST',
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        userId: 1,
+                        showId: 0,
+                        seatCount: 5
+                    }),
+                });
+
+                expect(res.status).toBe(401);
             }
         });
     });
--- base-concert-venue/__tests__/api/users.test.ts
+++ base-concert-venue/__tests__/api/users.test.ts
@@ -1,8 +1,10 @@
 import { testApiHandler } from 'next-test-api-route-handler';
 import usersHandler from '@/pages/api/users/index';
 import userReservationHandler from '@/pages/api/users/[userId]/reservations';
+import { validateToken } from '@/lib/auth/utils';
 
 jest.mock('@/lib/auth/utils');
+const validateTokenMock = validateToken as jest.Mock;
 
 describe('test for users', () => {
     it('POST /api/users receives token with correct credentials', async () => {
@@ -61,6 +63,32 @@ describe('test for users', () => {
 
                 const json = await res.json();
                 expect(json.userReservations).toHaveLength(0);
+            }
+        })
+    });
+
+    it("POST to /users/[userId]/reservations fails with 401", async () => {
+        validateTokenMock.mockResolvedValue(false);
+
+        await testApiHandler({
+            handler: userReservationHandler,
+            paramsPatcher: (params) => {
+                params.userId = 1;
+            },
+            test: async ({ fetch }) => {
+                const res = await fetch({
+                    method: 'POST',
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        userId: 1,
+                        showId: 0,
+                        seatCount: 5
+                    }),
+                });
+
+                expect(res.status).toBe(401);
             }
         })
     });
